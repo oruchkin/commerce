e522be170e09018d3bc5de2b4634af932e51ef63
oruchkin <72818824+oruchkin@users.noreply.github.com>
2021-01-28 22:04:32 +0300

    users to admin

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
 # Register your models here.
+
+#adding fields to admin()
+fields = list(UserAdmin.fieldsets)
+fields[0] = (None, {'fields': ('username', 'password')})
+UserAdmin.fieldsets = tuple(fields)
+
+admin.site.register(User, UserAdmin)
